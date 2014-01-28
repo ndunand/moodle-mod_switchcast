8503aad16cda66d01c124a7d3aefd4bf6662313f
Nicolas Dunand <nico@kolya.ch>
2014-01-28 15:12:54 +0100

    bumped version number

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version    = 2013121600;
+$module->version    = 2014010100;
 $module->requires   = 2011120500;       // Requires this Moodle version
 $module->maturity   = MATURITY_STABLE;
-$module->release    = '1.1';
+$module->release    = '1.2';
 
 $module->component  = 'mod_switchcast';     // Full name of the plugin (used for diagnostics)
 $module->cron       = 300;
