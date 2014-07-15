e541de3a650a51c159d4ceb173a58e0ee958b44f
Nicolas Dunand <nico@kolya.ch>
2014-07-15 14:43:57 +0200

    Added classes missing for class autoloading

---
+++ classes/event/clip_viewed.php
@@ -0,0 +1,101 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The mod_switchcast post created event.
+ *
+ * @package    mod_switchcast
+ * @copyright  2014 Dan Poltawski <dan@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_switchcast\event;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * The mod_switchcast post created event class.
+ *
+ * @property-read array $other {
+ *      Extra information about the event.
+ *
+ *      - int discussionid: The discussion id the post is part of.
+ *      - int switchcastid: The switchcast id the post is part of.
+ *      - string switchcasttype: The type of switchcast the post is part of.
+ * }
+ *
+ * @package    mod_switchcast
+ * @since      Moodle 2.7
+ * @copyright  2014 Dan Poltawski <dan@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class clip_viewed extends \core\event\base {
+    /**
+     * Init method.
+     *
+     * @return void
+     */
+    protected function init() {
+        $this->data['crud'] = 'r';
+        $this->data['edulevel'] = self::LEVEL_PARTICIPATING;
+        $this->data['objecttable'] = 'switchcast';
+    }
+
+    /**
+     * Returns description of what happened.
+     *
+     * @return string
+     */
+    public function get_description() {
+        $a = new \stdClass();
+        $a->userid = $this->userid;
+        $a->contextinstanceid = $this->contextinstanceid;
+        return get_string('event:clip_viewed_desc', 'switchcast', $a);
+    }
+
+    /**
+     * Return localised event name.
+     *
+     * @return string
+     */
+    public static function get_name() {
+        return get_string('event:clip_viewed', 'switchcast');
+    }
+
+    /**
+     * Get URL related to the action
+     *
+     * @return \moodle_url
+     */
+    public function get_url() {
+        return new \moodle_url('/mod/switchcast/view.php', array('id' => $this->contextinstanceid));
+    }
+
+    /**
+     * Return the legacy event log data.
+     *
+     * @return array|null
+     */
+    protected function get_legacy_logdata() {
+        // The legacy log table expects a relative path to /mod/switchcast/.
+        $logurl = substr($this->get_url()->out_as_local_url(), strlen('/mod/switchcast/'));
+
+        return array($this->courseid, 'switchcast', 'clip viewed', $logurl, $this->objectid, $this->contextinstanceid);
+    }
+
+
+}
+
