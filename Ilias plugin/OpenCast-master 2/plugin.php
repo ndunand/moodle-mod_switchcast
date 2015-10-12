3bfec175a393a1dc73c523313d84b6c9557b9966
Nicolas Dunand <nico@kolya.ch>
2015-10-12 10:50:11 +0200

    WIP port to OpenCast back-end

--- Ilias plugin/OpenCast-master 2/plugin.php
+++
@@ -1,8 +0,0 @@
-<?php
-$id = 'xoct';
-$version = '0.0.11';
-$ilias_min_version = '5.0.0';
-$ilias_max_version = '5.1.999';
-$responsible = 'Fabian Schmid';
-$responsible_mail = 'support@studer-raimann.ch';
-?>
